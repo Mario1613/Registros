c9ed89d436f5819cb9113eb58a636a4313486055
MarioAutomotriz <mario.gposan@gmail.com>
2023-07-07 06:35:03 -0600

    props

--- src/Component/AsideBarComponent.tsx
+++ src/Component/AsideBarComponent.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import { Link, useLocation } from 'react-router-dom';
 
 type RouteButton = {
     id: string;
@@ -12,42 +10,51 @@ type RouteButton = {
 interface Props {
     routeButtons: RouteButton[];
     title: string;
-    tamanio: string;
+    sidebarClick: boolean;
 }
+
 /**
- * 
- * @param tamanio es el ancho del sidebar.
- * @param title es el titulo principal
- * @param routeButtons es el arrray de objetos con propiedades de tipo id,title y route
- * @returns retorna unicamente el tsx
+ * @param sidebarClick activa el sidebar con un boolean
+ * @param title son los titulos del sidebar
+ * @param routeButtons son los botones laterales en forma de arreglo de objetos 
+ * @returns 
  */
-const AsideBarComponent = ({ tamanio, title, routeButtons }: Props) => {
-    const [active, setActive] = useState('0');
-
-    /**
-     * @param id sirve para ver que boton es el que se le aplicara la clase active
-     */
-    const handleItemClick = (id: string) => {
-        setActive(id);
-    };
-
+const AsideBarComponent = ({ sidebarClick, title, routeButtons }: Props) => {
+    const location = useLocation();
+    const currentPath = location.pathname;
     return (
-        <div className="d-none d-block d-sm-flex flex-column flex-shrink-0 p-3 bg-light h-100 position-absolute elevation-4" style={{ width: tamanio }}>
-            <Button className="position-absolute d-flex">X</Button>
-            <Link to="/" className="d-flex align-items-center mb-3 mb-md-0 link-dark text-decoration-none mt-4">
-                <span className="fs-2 d-flex text-center text-success fw-bold">{title}</span>
-            </Link>
-            <ul className="nav nav-pills flex-column mt-5 ">
-                {routeButtons.map((route: RouteButton) => (
-                    <li key={route.id} className="nav-item fs-5 fw-semibold mb-3" onClick={() => handleItemClick(route.id)}>
-                        <Link to={route.route} className={`nav-link ${active === route.id && 'active'} text-center`} aria-current="page">
-                            {route.title}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+        <div
+            className={`d-none d-block d-sm-flex flex-column flex-shrink-0 ${sidebarClick && 'p-3'} bg-light h-100 position-fixed elevation-4`}
+            style={{
+                marginLeft: `${sidebarClick ? '0%' : '-15%'}`,
+                width: '15%',
+                transition: 'margin .3s',
+                zIndex: '100'
+            }}
+        >
+            {sidebarClick && (
+                <>
+                    <Link to="/registro" className="d-flex align-items-center mb-3 mb-md-0 link-dark text-decoration-none mt-4">
+                        <span className="fs-2 d-flex text-center text-success fw-bold">{title}</span>
+                    </Link>
+                    <ul className="nav nav-pills flex-column mt-5">
+                        {routeButtons.map((route: RouteButton) => (
+                            <li key={route.id} className="nav-item fs-5 fw-semibold mb-3" >
+                                <Link
+                                    to={route.route}
+                                    className={`nav-link ${currentPath.startsWith(route.route) && 'active'} text-center`}
+                                    aria-current="page"
+                                >
+                                    {route.title}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </>
+            )}
         </div>
     );
 };
 
 export default AsideBarComponent;
+
