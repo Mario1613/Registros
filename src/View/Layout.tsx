45521a2e72fa5bca291f4d36852e35f954c716cc
MarioAutomotriz <mario.gposan@gmail.com>
2023-07-07 06:40:09 -0600

    correccion de rutas

--- src/View/Layout.tsx
+++ src/View/Layout.tsx
@@ -1,28 +1,38 @@
 import React from 'react'
 import NavbarComponent from '../Component/NavbarComponent'
 import AsideBarComponent from '../Component/AsideBarComponent'
-import { RootState } from '../store'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../store';
+import { setClickSidebar } from '../store/slices/context';
 
 const Layout = (props: any) => {
-    const tamanio = useSelector((state: RootState) => state.context.sidebar.tamanio)
+    const sidebarClick = useSelector((state: RootState) => state.context.sidebar)
+    const dispatch = useDispatch()
 
+    /**
+     * Hace que el sidebar derecho aparesca y desaparesca
+     */
+    const pushClick = () => dispatch(setClickSidebar(!sidebarClick))
+
+    /**
+     * Arreglo de objetos que depende el largo agrega botones de lado lateral en el sidebar
+     */
     let routeButtons = [
         {
             id: '0',
-            route: '/',
+            route: '/registro',
             title: 'Registro',
         },
         {
             id: '1',
-            route: "/lista",
+            route: "/registrados",
             title: "Registrados"
         }
     ]
     return (
         <>
-            <AsideBarComponent tamanio={tamanio} title={"Grupo Standex"} routeButtons={routeButtons} />
-            <NavbarComponent tamanio={tamanio} />
+            <AsideBarComponent sidebarClick={sidebarClick} title={"Grupo Standex"} routeButtons={routeButtons} />
+            <NavbarComponent sidebarClick={sidebarClick} pushClick={pushClick} />
             {props.children}
         </>
 
