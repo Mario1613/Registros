eba3af5e1564f6b7913c48de3296576f79d2876e
Mario1613 <mariojhaziel25@gmail.com>
2023-07-07 16:24:24 -0600

    fix bug

--- src/View/LoginGlobal/Main.tsx
+++ src/View/LoginGlobal/Main.tsx
@@ -12,68 +12,73 @@ import { changeViewForm } from '../../store/slices/context';
 import { useNavigate, useParams } from 'react-router-dom';
 
 const Main = () => {
-    const view_form = useSelector((state: RootState) => state.context.view_form);
-    const [userInfo, setUserInfo] = useState<any>();
-    const navigate = useNavigate();
-    const dispatch = useDispatch()
+  const view_form = useSelector((state: RootState) => state.context.view_form);
+  const [userInfo, setUserInfo] = useState<any>();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    //texto de contacto de la pagina de registro
-    let lists = [
-        { id: 1, text: 'Acapulco (744) 484 16 05, (744) 484 72 72' },
-        { id: 2, text: 'Cancún (998) 886 24 28' },
-        { id: 3, text: 'León (477) 311 81 62' },
-        { id: 4, text: 'Puerto Vallarta (322) 221 30 66' },
-        { id: 5, text: 'ventas@standex.com.mx' }
-    ];
-    //Verifica si existe algun dato en el localstorage
-    const idStorage = localStorage.getItem('idsesion');
-    const { id } = useParams();
-    const LOADINFO = async () => {
-        //si hay un dato redirecciona
-        if (idStorage) navigate(`/registro/${idStorage}`);
-        if ((!id && !idStorage) || id == null) dispatch(changeViewForm('registrar'))
-        if (id) {
-            //consulta la informacion en firebase
-            const responseInfo = await getRegistro(id);
-            if (responseInfo) {
-                const result = responseInfo.data();
-                if (result) {
-                    setUserInfo(result);
-                    dispatch(changeViewForm('registrado'))
-                }
-                if (!result) {
-                    localStorage.removeItem('idsesion')
-                    navigate('/registro');
-                }
-            }
+  //texto de contacto de la pagina de registro
+  let lists = [
+    { id: 1, text: "Acapulco (744) 484 16 05, (744) 484 72 72" },
+    { id: 2, text: "Cancún (998) 886 24 28" },
+    { id: 3, text: "León (477) 311 81 62" },
+    { id: 4, text: "Puerto Vallarta (322) 221 30 66" },
+    { id: 5, text: "ventas@standex.com.mx" },
+  ];
+  //Verifica si existe algun dato en el localstorage
+  const idStorage = localStorage.getItem("idsesion");
+  const { id } = useParams();
+  const LOADINFO = async () => {
+    //si hay un dato redirecciona
+    if (idStorage) navigate(`/registro/${idStorage}`);
+    if ((!id && !idStorage) || id == null)
+      dispatch(changeViewForm("registrar"));
+    if (id) {
+      //consulta la informacion en firebase
+      const responseInfo = await getRegistro(id);
+      if (responseInfo) {
+        const result = responseInfo.data();
+        if (result) {
+          setUserInfo(result);
+          dispatch(changeViewForm("registrado"));
         }
-    };
-    useEffect(() => {
-        LOADINFO();
-    }, []);
-    //Dependiendo si hay o datos en el storage mostrara un componente
-    const viewShow: any = {
-        registrado: <Registrado userInfo={userInfo} />,
-        registrar: <FormRegister LOADINFO={LOADINFO} />,
-    };
-    return (
-        <BackgroundWrapper style={{ backgroundImage: `url(${fondoImg})`, width: '100%' }}>
-            <ContainerWrapper>
-                <Row className="justify-content-center flex-md-row-reverse">
-                    <Col xs={12} md={8} lg={6} className='mt-4' >
-                        {viewShow[view_form]}
-                    </Col>
-                    <Col className='mt-4'>
-                        <ContactoComponent
-                            title="Contáctenos"
-                            paragraph="Para mayor información respecto a línea de stands y displays; o bien, a cualquiera de nuestros servicios."
-                            lists={lists}
-                        />
-                    </Col>
-                </Row>
-            </ContainerWrapper>
-        </BackgroundWrapper>
-    );
+        if (!result) {
+          localStorage.removeItem("idsesion");
+          navigate("/registro");
+        }
+      }
+    }
+  };
+  /* eslint-disable */
+  useEffect(() => {
+    LOADINFO();
+  }, []);
+  /* eslint-enable*/
+  //Dependiendo si hay o datos en el storage mostrara un componente
+  const viewShow: any = {
+    registrado: <Registrado userInfo={userInfo} />,
+    registrar: <FormRegister LOADINFO={LOADINFO} />,
+  };
+  return (
+    <BackgroundWrapper
+      style={{ backgroundImage: `url(${fondoImg})`, width: "100%" }}
+    >
+      <ContainerWrapper>
+        <Row className="justify-content-center flex-md-row-reverse">
+          <Col xs={12} md={8} lg={6} className="mt-4">
+            {viewShow[view_form]}
+          </Col>
+          <Col className="mt-4">
+            <ContactoComponent
+              title="Contáctenos"
+              paragraph="Para mayor información respecto a línea de stands y displays; o bien, a cualquiera de nuestros servicios."
+              lists={lists}
+            />
+          </Col>
+        </Row>
+      </ContainerWrapper>
+    </BackgroundWrapper>
+  );
 };
 
 const BackgroundWrapper = styled.div`
