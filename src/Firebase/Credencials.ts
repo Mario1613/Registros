40b820f98a405d03c5f1618b1c4b43fbe4d74501
MarioAutomotriz <mario.gposan@gmail.com>
2023-07-07 07:22:14 -0600

    delete incesesary

--- src/Firebase/Credencials.ts
+++ src/Firebase/Credencials.ts
@@ -1,4 +1,3 @@
-import React from "react";
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {
@@ -29,7 +28,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 
 const db = getFirestore();
