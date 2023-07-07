ed04b157f92096e1e80336cea067ddf649e43106
MarioAutomotriz <mario.gposan@gmail.com>
2023-07-06 22:33:26 -0600

    tipado y navegacion correcto

--- src/Component/NavbarComponent.tsx
+++ src/Component/NavbarComponent.tsx
@@ -1,14 +1,16 @@
 import React from "react"
 import Logo from "../assets/logo-blanco.png"
+import { Link } from "react-router-dom"
 
+interface Props {
+    tamanio: string
+}
 
-
-const NavbarComponent = ({ tamanio }: any) => {
+const NavbarComponent = ({ tamanio }: Props) => {
     return (
         <nav className="navbar navbar-light bg-success fixed-top " style={{ marginLeft: tamanio, maxWidth: '100%' }}>
             <div className="container-fluid justify-content-center">
-                <a href="/"><img src={Logo} width="90%" alt="" /></a>
-
+                <Link to='/'><img src={Logo} width="90%" alt="Logo Standex" /></Link>
             </div>
         </nav>
     )
