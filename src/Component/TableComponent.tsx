161f619ec579979e6e528ae2d09cc2cd00908de2
Mario1613 <mariojhaziel25@gmail.com>
2023-07-08 04:52:14 -0600

    paginacion en tabla

--- src/Component/TableComponent.tsx
+++ src/Component/TableComponent.tsx
@@ -1,123 +1,195 @@
-import React from 'react';
-import ModalDelete from '../View/ListRegisterGlobal/ModalDelete';
-import { useState } from 'react';
-import ModalEdit from '../View/ListRegisterGlobal/ModalEdit';
+import React, { useState } from "react";
+import ModalDelete from "../View/ListRegisterGlobal/ModalDelete";
+import ModalEdit from "../View/ListRegisterGlobal/ModalEdit";
 
 type TableColumn = {
-    label: string;
-    dataKey: string;
+  label: string;
+  dataKey: string;
 };
 
 type TableData = {
-    [key: string]: string | number;
+  [key: string]: string | number;
 };
 
 interface Props {
-    columns: TableColumn[];
-    data: any;
-    deleteRegistro: (e: string) => void;
-    updateRegistro: any
-
+  columns: TableColumn[];
+  data: any;
+  deleteRegistro: (e: string) => void;
+  updateRegistro: any;
 }
 
-const TableComponent: React.FC<Props> = ({ columns, data, deleteRegistro, updateRegistro }) => {
+const TableComponent: React.FC<Props> = ({
+  columns,
+  data,
+  deleteRegistro,
+  updateRegistro,
+}) => {
+  const [registro, setRegistro] = useState<any>();
+  const [isActiveModalDelete, setIsActiveModalDelete] =
+    useState<boolean>(false);
+  const [isActiveModalEdit, setIsActiveModalEdit] = useState<boolean>(false);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const itemsPerPage = 12;
 
-    const [registro, setRegistro] = useState<any>()
-    const [isActiveModalDelete, setIsActiveModalDelete] = useState<boolean>(false)
-    const [isActiveModalEdit, setIsActiveModalEdit] = useState<boolean>(false)
-    const handleEditForm = (row: TableData) => {
-        setRegistro(row)
-        setIsActiveModalEdit(true)
+  const handleEditForm = (row: TableData) => {
+    setRegistro(row);
+    setIsActiveModalEdit(true);
+  };
+
+  const confirmEditModal = (values: any) => {
+    let newFilds = {
+      name: values.name,
+      dateOfBirth: values.dateOfBirth,
+      creditCardNumber: values.creditCardNumber,
+      youtubeChannel: values.youtubeChannel,
     };
-    const confirmEditModal = (values: any) => {
-        let newFilds = {
-            name: values.name,
-            dateOfBirth: values.dateOfBirth,
-            creditCardNumber: values.creditCardNumber,
-            youtubeChannel: values.youtubeChannel
-        }
-        updateRegistro(values.id, newFilds)
-        setIsActiveModalEdit(false)
-    }
-    const cancelEditModal = () => {
-        setIsActiveModalEdit(false)
+    updateRegistro(values.id, newFilds);
+    setIsActiveModalEdit(false);
+  };
 
-    }
+  const cancelEditModal = () => {
+    setIsActiveModalEdit(false);
+  };
 
+  const handleDeleteForm = (row: TableData) => {
+    setRegistro(row);
+    setIsActiveModalDelete(true);
+  };
 
+  const confirmDeleteModal = (values: any) => {
+    deleteRegistro(values.id);
+    setIsActiveModalDelete(false);
+  };
 
+  const cancelDeleteModal = () => {
+    setIsActiveModalDelete(false);
+    setRegistro({});
+  };
 
+  // Calcula el índice del último elemento de la página actual
+  const indexOfLastItem = currentPage * itemsPerPage;
+  // Calcula el índice del primer elemento de la página actual
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  // Obtiene los elementos de la página actual
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
 
+  // Calcula el número total de páginas
+  const totalPages = Math.ceil(data.length / itemsPerPage);
 
+  // Genera los números de página
+  const pageNumbers = [];
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  }
 
-    //Funcion que activa el modal, el el boton que aparece de eliminar en la tabla
-    const handleDeleteForm = (row: TableData) => {
-        // Lógica para eliminar el elemento
-        setRegistro(row)
-        setIsActiveModalDelete(true)
-    };
-    //Funciones del Modal
-    const confirmDeleteModal = (values: any) => {
-        deleteRegistro(values.id)
-        setIsActiveModalDelete(false)
+  // Cambia la página actual
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
 
-    }
-    const cancelDeleteModal = () => {
-        setIsActiveModalDelete(false)
-        setRegistro({})
-    }
+  return (
+    <div
+      style={{
+        overflow: "hidden",
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100%",
+        height: "70vh",
+        backgroundColor: "white",
+        borderRadius: "1rem",
+      }}
+    >
+      <div
+        style={{
+          height: "100%",
+          overflowY: "scroll",
+          padding: "0 1rem 0 1rem",
+        }}
+      >
+        <table className="table table-hover">
+          <thead>
+            <tr
+              style={{
+                position: "sticky",
+                top: 0,
+                background: "#f8f9fa",
+                zIndex: 1,
+              }}
+            >
+              {columns.map((column, index) => (
+                <th key={index} scope="col">
+                  {column.label}
+                </th>
+              ))}
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {currentItems.map((row: TableData, rowIndex: number) => (
+              <tr key={rowIndex}>
+                {columns.map((column, columnIndex) => (
+                  <td key={columnIndex}>{row[column.dataKey]}</td>
+                ))}
+                <td>
+                  <button
+                    className="btn btn-primary"
+                    onClick={() => handleEditForm(row)}
+                  >
+                    Editar
+                  </button>
+                  <button
+                    className="btn btn-danger ms-2"
+                    onClick={() => handleDeleteForm(row)}
+                  >
+                    Eliminar
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
 
-    return (
-        <div style={{ maxHeight: '100%', overflowY: 'auto' }}>
-            <table className="table table-hover">
-                <thead>
-                    <tr style={{ position: 'sticky', top: 0, background: '#f8f9fa', zIndex: 1 }}>
-                        {columns.map((column, index) => (
-                            <th key={index} scope="col">
-                                {column.label}
-                            </th>
-                        ))}
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data.map((row: TableData, rowIndex: number) => (
-                        <tr key={rowIndex}>
-                            {columns.map((column, columnIndex) => (
-                                <td key={columnIndex}>{row[column.dataKey]}</td>
-                            ))}
-                            <td>
-                                <button className="btn btn-primary" onClick={() => handleEditForm(row)}>
-                                    Editar
-                                </button>
-                                <button className="btn btn-danger ms-2" onClick={() => handleDeleteForm(row)}>
-                                    Eliminar
-                                </button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-            {isActiveModalDelete && (
-                <ModalDelete
-                    titleModal="El elemento se eliminara para siempre, Estas seguro?"
-                    confirmDeleteModal={confirmDeleteModal}
-                    cancelDeleteModal={cancelDeleteModal}
-                    isActiveModalDelete={isActiveModalDelete}
-                    registro={registro}
-                />
-            )}
-            {isActiveModalEdit && (
-                <ModalEdit
-                    titleModal="Verifique los datos antes de guardar"
-                    isActiveModalEdit={isActiveModalEdit}
-                    confirmEditModal={confirmEditModal}
-                    cancelEditModal={cancelEditModal}
-                    registro={registro}
-                />
-            )}
-        </div>
-    );
+        {/* Renderiza los modales */}
+        {isActiveModalDelete && (
+          <ModalDelete
+            titleModal="El elemento se eliminará para siempre. ¿Estás seguro?"
+            confirmDeleteModal={confirmDeleteModal}
+            cancelDeleteModal={cancelDeleteModal}
+            isActiveModalDelete={isActiveModalDelete}
+            registro={registro}
+          />
+        )}
+        {isActiveModalEdit && (
+          <ModalEdit
+            titleModal="Verifique los datos antes de guardar"
+            isActiveModalEdit={isActiveModalEdit}
+            confirmEditModal={confirmEditModal}
+            cancelEditModal={cancelEditModal}
+            registro={registro}
+          />
+        )}
+      </div>
+      {/* Renderiza los números de página */}
+      <nav
+        style={{
+          marginLeft: "1rem",
+        }}
+      >
+        <ul className="pagination">
+          {pageNumbers.map((pageNumber) => (
+            <li key={pageNumber} className="page-item">
+              <button
+                className="page-link"
+                onClick={() => paginate(pageNumber)}
+              >
+                {pageNumber}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default TableComponent;
