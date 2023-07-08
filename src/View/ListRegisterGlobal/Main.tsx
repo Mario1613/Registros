d4bfa707019a9ab30344bac128807d7b29c77305
Mario1613 <mariojhaziel25@gmail.com>
2023-07-08 04:52:27 -0600

    show table

--- src/View/ListRegisterGlobal/Main.tsx
+++ src/View/ListRegisterGlobal/Main.tsx
@@ -1,66 +1,80 @@
-import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import styled from "styled-components";
-import fondoImg from '../../assets/fondo.jpg';
-import TableComponent from '../../Component/TableComponent';
-import { onGetRegistros, deleteRegistro, updateRegistro } from '../../Firebase/Credencials';
-import { useState, useEffect } from 'react';
-import { Registro } from '../../models/GlobalTypes';
-
-
+import fondoImg from "../../assets/fondo.jpg";
+import TableComponent from "../../Component/TableComponent";
+import {
+  onGetRegistros,
+  deleteRegistro,
+  updateRegistro,
+} from "../../Firebase/Credencials";
+import { useState, useEffect } from "react";
+import { Registro } from "../../models/GlobalTypes";
 
 const Main = () => {
-    // Arreglo de objetos, esto podría ser desde un enpoint o algo similar
-    const [registros, setRegistros] = useState<Registro[]>([]);
-    const columns = [
-        { label: 'Nombre', dataKey: 'name' },
-        { label: 'Fecha de Nacimiento', dataKey: 'dateOfBirth' },
-        { label: 'Numero de Tarjeta', dataKey: 'creditCardNumber' },
-        { label: 'URL', dataKey: 'youtubeChannel' }
-    ];
+  // Arreglo de objetos, esto podría ser desde un enpoint o algo similar
+  const [registros, setRegistros] = useState<Registro[]>([]);
+  const columns = [
+    { label: "Nombre", dataKey: "name" },
+    { label: "Fecha de Nacimiento", dataKey: "dateOfBirth" },
+    { label: "Numero de Tarjeta", dataKey: "creditCardNumber" },
+    { label: "URL", dataKey: "youtubeChannel" },
+  ];
 
-    useEffect(() => {
-        const unsubscribe = onGetRegistros((snapshot: any) => {
-            const nuevosRegistros = snapshot.docs.map((doc: any) => ({
-                id: doc.id,
-                ...doc.data(),
-            }));
-            setRegistros(nuevosRegistros);
-        });
+  useEffect(() => {
+    const unsubscribe = onGetRegistros((snapshot: any) => {
+      const nuevosRegistros = snapshot.docs.map((doc: any) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setRegistros(nuevosRegistros);
+    });
 
-        return () => unsubscribe(); // Limpia el listener cuando el componente se desmonta
-    }, []);
+    return () => unsubscribe(); // Limpia el listener cuando el componente se desmonta
+  }, []);
 
-
-    return (
-        <BackgroundWrapper style={{ backgroundImage: `url(${fondoImg})`, width: '100%' }}>
-            <ContainerWrapper>
-                <Row className="justify-content-center flex-md-row-reverse">
-                    <Col xs={12} md={12} lg={12} className='mt-4' style={{ maxHeight: '70vh' }} >
-                        <TableContainer>
-                            <TableComponent columns={columns} data={registros} deleteRegistro={deleteRegistro} updateRegistro={updateRegistro} />
-                        </TableContainer>
-                    </Col>
-                </Row>
-            </ContainerWrapper>
-        </BackgroundWrapper>
-    );
+  return (
+    <BackgroundWrapper
+      style={{ backgroundImage: `url(${fondoImg})`, width: "100%" }}
+    >
+      <ContainerWrapper>
+        <Row className="justify-content-center flex-md-row-reverse">
+          <Col
+            xs={12}
+            md={12}
+            lg={12}
+            className="mt-4"
+            style={{ maxHeight: "70vh" }}
+          >
+            <TableContainer>
+              <TableComponent
+                columns={columns}
+                data={registros}
+                deleteRegistro={deleteRegistro}
+                updateRegistro={updateRegistro}
+              />
+            </TableContainer>
+          </Col>
+        </Row>
+      </ContainerWrapper>
+    </BackgroundWrapper>
+  );
 };
 
 const BackgroundWrapper = styled.div`
-  height: 100%;
-  @media(min-width: 900px){
+  height: 100vh;
+  @media (min-width: 900px) {
     height: 100vh;
   }
   width: 100%;
-   background-size: cover;
+  background-size: cover;
 `;
 
 const ContainerWrapper = styled(Container)`
   display: flex;
   flex-direction: column;
   margin-top: 11%;
-  @media(max-width: 768px){
+  @media (max-width: 768px) {
     margin-top: 32%;
   }
 `;
